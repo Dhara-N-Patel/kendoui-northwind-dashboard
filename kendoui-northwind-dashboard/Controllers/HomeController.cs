02fbb82b07a9ea81862f44402865835fb69c6a8c
dhara <dhara@DESKTOP-RCQJKQV>
2018-05-15 22:15:16 -0500

    Cleaned Solution

--- kendoui-northwind-dashboard/Controllers/HomeController.cs
+++ kendoui-northwind-dashboard/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Kendo.Mvc.UI;
-using KendoUI.Northwind.Dashboard.Models;
 using System.Collections.Generic;
 using System.Linq; 
 using System.Web.Mvc;
@@ -45,6 +44,10 @@ namespace KendoUI.Northwind.Dashboard.Controllers
         {
             return View();
         }
+        public ActionResult Inspections()
+        {
+            return View();
+        }
         public ActionResult PaymentRequests()
         {
             return View();
@@ -56,75 +59,6 @@ namespace KendoUI.Northwind.Dashboard.Controllers
         public ActionResult Closeout()
         {
             return View();
-        }
-        
-        public ActionResult Products_Read(string text)
-        {
-            var northwind = new NorthwindEntities();
-
-            var products = northwind.Products.Select(product => new ProductViewModel
-            {
-                CategoryID = product.CategoryID,
-                ProductID = product.ProductID,
-                ProductName = product.ProductName,
-                UnitPrice = product.UnitPrice ?? 0,
-                UnitsInStock = product.UnitsInStock ?? 0,
-                UnitsOnOrder = product.UnitsOnOrder ?? 0,
-                Discontinued = product.Discontinued
-            });
-
-            if (!string.IsNullOrEmpty(text))
-            {
-                products = products.Where(p => p.ProductName.Contains(text));
-            }
-
-            return Json(products, JsonRequestBehavior.AllowGet);
-        }
-
-        public static IEnumerable<EmployeeViewModel> GetEmployees()
-        {
-            var employees = new NorthwindEntities().Employees.Select(e => new EmployeeViewModel
-            {
-                EmployeeID = e.EmployeeID,
-                EmployeeName = e.FirstName + " " + e.LastName
-            }).OrderBy(e => e.EmployeeName);
-
-            return employees;
-        }
-
-        public IEnumerable<ProductViewModel> GetProducts()
-        {
-            var products = new NorthwindEntities().Products.Select(e => new ProductViewModel
-            {
-                ProductID = e.ProductID,
-                ProductName = e.ProductName
-            }).OrderBy(e => e.ProductName);
-
-            return products;
-        }
-
-
-        public static IQueryable<CustomerViewModel> GetCustomers()
-        {
-            var customers = new NorthwindEntities().Customers.Select(e => new CustomerViewModel
-            {
-                CustomerID = e.CustomerID,
-                CompanyName = e.CompanyName
-            }).OrderBy(e => e.CompanyName);
-
-            return customers;
-        }
-
-
-        public IQueryable<ShipperViewModel> GetShippers()
-        {
-            var shippers = new NorthwindEntities().Shippers.Select(e => new ShipperViewModel
-            {
-                ShipperID = e.ShipperID,
-                CompanyName = e.CompanyName
-            }).OrderBy(e => e.CompanyName);
-
-            return shippers;
         }
     }
 }
