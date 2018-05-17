8ed3915cf2c87b21889e15407c226ce74b25c43d
dhara <dhara@DESKTOP-RCQJKQV>
2018-05-17 10:49:14 -0500

    few more changes

---
+++ kendoui-northwind-dashboard/Scripts/Contracts.js
@@ -0,0 +1,149 @@
+﻿$("#Other1").click(function () {
+    $("#Other").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#LegalFees1").click(function () {
+    $("#LegalFees").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+});
+
+
+$("#Contingency1").click(function () {
+    $("#Contingency").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#Construction1").click(function () {
+    $("#Construction").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#Land1").click(function () {
+    $("#Land").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#Permits1").click(function () {
+    $("#Permits").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#ArchitectureEngineeringFees1").click(function () {
+    $("#ArchitectureEngineeringFees").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#InterestDuringConstruction1").click(function () {
+    $("#InterestDuringConstruction").css({ display: '' });
+
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#AccountingChanges1").click(function () {
+    $("#AccountingChanges").css({ display: '' });
+    //hide other tables
+    $("#AdministrativeCosts").css({ display: 'none' });
+    $("#Permits").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
+
+$("#AdministrativeCosts1").click(function () {
+    $("#AdministrativeCosts").css({ display: '' });
+
+    //hide other tables
+    $("#Permits").css({ display: 'none' });
+    $("#AccountingChanges").css({ display: 'none' });
+    $("#InterestDuringConstruction").css({ display: 'none' });
+    $("#ArchitectureEngineeringFees").css({ display: 'none' });
+    $("#Land").css({ display: 'none' });
+    $("#Construction").css({ display: 'none' });
+    $("#Contingency").css({ display: 'none' });
+    $("#Other").css({ display: 'none' });
+    $("#LegalFees").css({ display: 'none' });
+});
